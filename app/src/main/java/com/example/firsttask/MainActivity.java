e858b54225065d80a3a4ace113ade518c5708bfa
nora alobaidi <101515331+noraalobaidi@users.noreply.github.com>
2023-02-14 12:44:04 +0300

    WORKING IBAN validator API

--- app/src/main/java/com/example/firsttask/MainActivity.java
+++ app/src/main/java/com/example/firsttask/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.firsttask;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+    TextView data;
+    String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        data =findViewById(R.id.data);
+        url="https://api.apilayer.com/bank_data/iban_validate?iban_number=DE89370400440532013000&apikey=CUDJOz7iGPwQXHOQVJ8BSBIOQyxj5iX2";
+        JsonObjectRequest request= new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try{
+                    String IBANvalidtag= response.getString("valid");
+                    data.setText(IBANvalidtag);
+
+                }
+                catch(Exception error){
+
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        Volley.newRequestQueue(this).add(request);
+
+
     }
 }
